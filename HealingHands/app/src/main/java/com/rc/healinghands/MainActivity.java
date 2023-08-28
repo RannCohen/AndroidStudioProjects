317d2ccb086619adddc48cfe02b4235c96655d05
ranovo <ranchn1377@gmail.com>
2023-08-28 21:17:21 +0300

    more refactor added reset button

--- HealingHands/app/src/main/java/com/rc/healinghands/MainActivity.java
+++ HealingHands/app/src/main/java/com/rc/healinghands/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,13 +19,13 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = MainActivity.class.getSimpleName();
-    LinearLayout llTextInput, llIntervalInput, llMainActivity;
+    LinearLayout llMinutesLine, llIntervalLine, llMainActivity, llButtons;
     TextView tvTimer;
-    ImageView ivHands;
-    EditText etTimeInput, etIntervalInput;
-    Button btnStart;
-    boolean buttonMode = true; // true = start, false = stop
+    EditText etMinuteInput, etIntervalInput;
+    Button btnStartPause, btnReset;
+    boolean isTimerRunning = false; // true = show stop button , false = show start button
     CountDownTimer timer;
+
     long TimeBuff, UpdateTime = 0L, timeInterval = 0L;
     int Seconds, Minutes;
 
@@ -34,111 +33,94 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Log.i(TAG, "onCreate");
-
         NotificationManagerCompat.from(this).cancelAll();
+        initXml();
+        playWelcome();
+    }
+    
+    View.OnClickListener buttonsListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == btnStartPause.getId()) {
+                if (!isTimerRunning) {
+                    if (!(etMinuteInput.getText().toString().isEmpty())) {
+                        isTimerRunning = true;
+                        btnStartPause.setText(R.string.stop);
+                        btnStartPause.setBackgroundColor(getResources().getColor(R.color.red));
+                        long timeInput = Long.parseLong(etMinuteInput.getText().toString()) * 1000 * 60;
+                        if (!etIntervalInput.getText().toString().isEmpty()) {
+                            timeInterval = Long.parseLong(etIntervalInput.getText().toString());
+                        }
+                        Log.i(TAG, "Time input = " + Long.parseLong(etMinuteInput.getText().toString()) + " minutes");
+                        Log.i(TAG, "Time intervals = " + timeInterval + " Minutes");
+                        timer = new CountDownTimer(timeInput, 1000) {
+                            public void onTick(long millisUntilFinished) {
+                                UpdateTime = TimeBuff + millisUntilFinished;
+                                Seconds = (int) (UpdateTime / 1000);
+                                Minutes = Seconds / 60;
+                                Seconds = Seconds % 60;
+                                if ((timeInterval != 0) && (Minutes % timeInterval == 0) && (Seconds == 0)) {
+                                    playSmallDing();
+                                }
+                                String displayTime = String.format(Locale.getDefault(), "%02d", Minutes) + ":" + String.format(Locale.getDefault(), "%02d", Seconds);
+                                tvTimer.setText(displayTime);
+                                Log.i(TAG, "Display Time: " + displayTime);
+                            }
 
+                            public void onFinish() {
+                                String done = "Done!";
+                                tvTimer.setText(done);
+                                Log.i(TAG, done);
+                                playBigDing();
+                                isTimerRunning = false;
+                                btnStartPause.setText(R.string.start);
+                                btnStartPause.setBackgroundColor(getResources().getColor(R.color.green));
+                            }
+                        }.start();
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast toast = Toast.makeText(getApplicationContext(), R.string.input_healing_time, Toast.LENGTH_LONG);
+                                toast.setGravity(Gravity.BOTTOM, 0, 250);
+                                toast.show();
+                            }
+                        });
+                    }
+                } else {
+                    isTimerRunning = false;
+                    btnStartPause.setText(R.string.start);
+                    btnStartPause.setBackgroundColor(getResources().getColor(R.color.green));
+                    timer.cancel();
+                    tvTimer.setText("");
+                }
+            } else if (v.getId() == btnReset.getId()) {
+
+
+            }
+        }
+    };
+
+    private void initXml() {
         llMainActivity = findViewById(R.id.llMainActivity);
-        llTextInput = findViewById(R.id.llMinutesInput);
-        llIntervalInput = findViewById(R.id.llIntervalInput);
         tvTimer = findViewById(R.id.tvTimer);
-        ivHands = findViewById(R.id.ivHands);
-        btnStart = findViewById(R.id.btnStartPause);
-        etTimeInput = findViewById(R.id.etTimeInput);
+        llMinutesLine = findViewById(R.id.llMinutesLine);
+        etMinuteInput = findViewById(R.id.etMinuteInput);
+        llIntervalLine = findViewById(R.id.llIntervalLine);
         etIntervalInput = findViewById(R.id.etIntervalInput);
-
+        llButtons = findViewById(R.id.llButtons);
+        btnReset = findViewById(R.id.btnReset);
+        btnReset.setOnClickListener(buttonsListener);
+        btnStartPause = findViewById(R.id.btnStartPause);
+        btnStartPause.setOnClickListener(buttonsListener);
+    }
+    
+    private void playWelcome() {
         MediaPlayer shalom = MediaPlayer.create(MainActivity.this, R.raw.shalom);
         shalom.start();
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (buttonMode) {
-                            if (!(etTimeInput.getText().toString().isEmpty())) {
-                                buttonMode = false;
-                                btnStart.setText(R.string.stop);
-                                btnStart.setBackgroundColor(getResources().getColor(R.color.red));
-                                llTextInput.setVisibility(View.GONE);
-                                llIntervalInput.setVisibility(View.GONE);
-                                ivHands.setVisibility(View.GONE);
-                                tvTimer.setBackgroundColor(getResources().getColor(R.color.black));
-                                llMainActivity.setBackgroundColor(getResources().getColor(R.color.black));
-                                tvTimer.setTextColor(getResources().getColor(R.color.white));
-                                tvTimer.setVisibility(View.VISIBLE);
-                                long timeInput = Long.parseLong(etTimeInput.getText().toString()) * 1000 * 60;
-                                if (!etIntervalInput.getText().toString().isEmpty()) {
-                                    timeInterval = Long.parseLong(etIntervalInput.getText().toString());
-                                }
-                                Log.i(TAG, "Time input = " + Long.parseLong(etTimeInput.getText().toString()) + " minutes");
-                                Log.i(TAG, "Time intervals = " + timeInterval + " Minutes");
-                                timer = new CountDownTimer(timeInput, 1000) {
-                                    public void onTick(long millisUntilFinished) {
-                                        UpdateTime = TimeBuff + millisUntilFinished;
-                                        Seconds = (int) (UpdateTime / 1000);
-                                        Minutes = Seconds / 60;
-                                        Seconds = Seconds % 60;
-                                        if ((timeInterval != 0) && (Minutes % timeInterval == 0) && (Seconds == 0)) {
-                                            playSmallDing();
-                                        }
-                                        String displayTime = String.format(Locale.getDefault(), "%02d", Minutes) + ":" + String.format(Locale.getDefault(), "%02d", Seconds);
-                                        tvTimer.setText(displayTime);
-                                        Log.i(TAG, "Display Time: " + displayTime);
-                                    }
-
-                                    public void onFinish() {
-                                        String done = "Done!";
-                                        tvTimer.setText(done);
-                                        Log.i(TAG, done);
-                                        playBigDing();
-                                        buttonMode = true;
-                                        btnStart.setText(R.string.start);
-                                        btnStart.setBackgroundColor(getResources().getColor(R.color.green));
-                                        tvTimer.setBackgroundColor(getResources().getColor(R.color.white));
-                                        llMainActivity.setBackgroundColor(getResources().getColor(R.color.white));
-                                        tvTimer.setTextColor(getResources().getColor(R.color.black));
-                                        llTextInput.setVisibility(View.VISIBLE);
-                                        llIntervalInput.setVisibility(View.VISIBLE);
-                                        ivHands.setVisibility(View.VISIBLE);
-                                    }
-                                }.start();
-                            } else {
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Toast toast = Toast.makeText(getApplicationContext(), R.string.input_healing_time, Toast.LENGTH_LONG);
-                                        toast.setGravity(Gravity.BOTTOM, 0, 250);
-                                        toast.show();
-                                    }
-                                });
-                            }
-                        } else {
-                            llTextInput.setVisibility(View.VISIBLE);
-                            llIntervalInput.setVisibility(View.VISIBLE);
-                            ivHands.setVisibility(View.VISIBLE);
-                            tvTimer.setBackgroundColor(getResources().getColor(R.color.white));
-                            llMainActivity.setBackgroundColor(getResources().getColor(R.color.white));
-                            tvTimer.setTextColor(getResources().getColor(R.color.black));
-                            tvTimer.setVisibility(View.GONE);
-                            buttonMode = true;
-                            btnStart.setText(R.string.start);
-                            btnStart.setBackgroundColor(getResources().getColor(R.color.green));
-                            timer.cancel();
-                            tvTimer.setText("");
-                        }
-                    }
-                });
-            }
-        });
-    }
-
+    
     private void playBigDing() {
         Log.i(TAG, "Big Ding");
         MediaPlayer big_ding = MediaPlayer.create(MainActivity.this, R.raw.big_ding);
